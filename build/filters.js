b5b91d0aabdfd7798426821f5000aea4cee59c79
Augusto Samussone <augustooalcides@gmail.com>
2024-09-30 18:44:29 +0100

    Rename BlueberryAi -> KiwiAI

--- build/filters.js
+++ build/filters.js
@@ -26,7 +26,7 @@ module.exports.all = [
 	'!out*/**',
 	'!test/**/out/**',
 	'!**/node_modules/**',
-	'!**/extensions/blueberryai-extension/**'
+	'!**/extensions/kiwiai-extension/**'
 ];
 
 module.exports.unicodeFilter = [
